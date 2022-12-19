baeb4613b68e4b22ea1a467f4e84c8a4c98db012
BhaveshKumar <bk769221@gmail.com>
2022-12-19 11:31:11 +0530

    final Change

--- src/Components/Pages/BrandCardPage.jsx
+++ src/Components/Pages/BrandCardPage.jsx
@@ -1,7 +1,7 @@
 
 import Footer from "../Footer";
 import Navbar from "../Navbar";
-import  {SimpleGrid, GridItem, Box} from "@chakra-ui/react";
+import  {SimpleGrid} from "@chakra-ui/react";
 let imageData = [
   {
     image:
--- src/Components/Pages/SingleProductPage.jsx
+++ src/Components/Pages/SingleProductPage.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import {
   Stack,
@@ -10,13 +11,31 @@ import {
   Text,
   CardFooter,
   Button,
-  ButtonGroup,
-  Divider,
+  HStack,
 } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
+
 import Navbar from "../Navbar";
 import ImageSlider from "../../Crousel/MainSlider";
 import Footer from "../Footer";
 function SingleProductPage() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+  const cancelRef = React.useRef();
   const { id } = useParams();
   const [data, setData] = useState({});
   const [price, setPrice] = useState(1);
@@ -29,6 +48,16 @@ function SingleProductPage() {
   useEffect(() => {
     getData();
   }, [id]);
+  const handleClick = () => {
+    onClose()
+    navigate("/");
+  };
+  // const handleAlert = ()=>{
+  //   return  <Alert status='success'>
+  //   <AlertIcon />
+  //  Your Order is Placed ... Thank You...Shop More
+  // </Alert>
+  // }
   console.log("datta", data);
   return (
     <>
@@ -68,7 +97,11 @@ function SingleProductPage() {
               -
             </Button>{" "}
             <Button marginTop={"25px"}> {price} </Button>{" "}
-            <Button marginTop={"25px"} onClick={() => setPrice(price + 1)} border="1px black solid">
+            <Button
+              marginTop={"25px"}
+              onClick={() => setPrice(price + 1)}
+              border="1px black solid"
+            >
               +
             </Button>
             <br />
@@ -83,9 +116,84 @@ function SingleProductPage() {
                 bg: "white",
                 border: "1px black solid",
               }}
+              onClick={onOpen}
             >
               Buy Now
             </Button>
+            <AlertDialog
+              isOpen={isOpen}
+              leastDestructiveRef={cancelRef}
+              onClose={onClose}
+            >
+              <AlertDialogOverlay>
+                <AlertDialogContent>
+                  <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                    <HStack>
+                      <Text
+                        fontFamily={"cursive"}
+                        fontSize="25px"
+                        color={"#e28743"}
+                      >
+                        {" "}
+                        Order Placed{" "}
+                      </Text>
+
+                      <img src="https://img.icons8.com/external-stickers-smashing-stocks/70/null/external-Done-success-and-error-messages-stickers-smashing-stocks.png" />
+                    </HStack>
+                  </AlertDialogHeader>
+
+                  <AlertDialogBody>
+                    <Alert
+                      status="success"
+                      variant="subtle"
+                      flexDirection="column"
+                      alignItems="center"
+                      justifyContent="center"
+                      textAlign="center"
+                      height="200px"
+                    >
+                      <AlertIcon boxSize="40px" mr={0} />
+                      <AlertTitle mt={4} mb={1} fontSize="lg">
+                        Your Order is Placed !
+                      </AlertTitle>
+                      <AlertDescription maxWidth="sm">
+                        Thanks for Shoping at Shoppers Hub please continue
+                        shoping
+                      </AlertDescription>
+                    </Alert>
+                  </AlertDialogBody>
+
+                  <AlertDialogFooter>
+                    <Button
+                      ref={cancelRef}
+                      onClick={onClose}
+                      _hover={{
+                        color: "black",
+                        bg: "white",
+                        border: "1px black solid",
+                      }}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      bg="#88d6e2"
+                      onClick={() => {
+                        onClose();
+                        handleClick()
+                      }}
+                      ml={3}
+                      _hover={{
+                        color: "black",
+                        bg: "white",
+                        border: "1px black solid",
+                      }}
+                    >
+                      OK
+                    </Button>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialogOverlay>
+            </AlertDialog>
           </CardBody>
 
           <CardFooter></CardFooter>
